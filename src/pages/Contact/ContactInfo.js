777ddc666c65cad17c207f2c6f941066451ed654
JadenFicklin <jadendficklin@gmail.com>
2023-04-02 14:21:00 -0600

    form validation

--- src/pages/Contact/ContactInfo.js
+++ src/pages/Contact/ContactInfo.js
@@ -1,11 +1,117 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import c from 'classnames';
+import emailjs from '@emailjs/browser';
 
 import '../../App.css';
-import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+import {
+    BsFillArrowUpRightCircleFill,
+    BsFillCheckCircleFill
+} from 'react-icons/bs';
+
+const requiredFields = [
+    { name: 'user_first_name', label: 'First Name' },
+    { name: 'user_last_name', label: 'Last Name' },
+    { name: 'email_address', label: 'Email Address' },
+    { name: 'phone_number', label: 'Phone Number' },
+    { name: 'message', label: 'Message' }
+];
+
+const FormInput = ({ name, type, label, errors, onChange, className }) => (
+    <input
+        onChange={onChange}
+        name={name}
+        type={type}
+        className={c(`my-2 border-b-[1px] pb-3 ${className}`, {
+            'border-b-[#b3b3b3]': errors[name],
+            'border-b-black': !errors[name]
+        })}
+        placeholder={label}
+    />
+);
 
 function ContactInfo() {
+    const form = useRef();
+    const [errors, setErrors] = useState({});
+    const [isValid, setIsValid] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [showMissingFieldsMessage, setShowMissingFieldsMessage] =
+        useState(false);
+
+    const messageSent = () => {
+        setSuccess(true);
+        setTimeout(() => {
+            setSuccess(false);
+        }, 3000);
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        if (!validateForm()) {
+            setShowMissingFieldsMessage(true);
+            return;
+        } else {
+            setShowMissingFieldsMessage(false);
+        }
+
+        emailjs
+            .sendForm(
+                'service_m80g8po',
+                'template_f65y2en',
+                form.current,
+                'PI5SHpV0KCPvYoK41'
+            )
+            .then(
+                (result) => {
+                    messageSent();
+                    console.log(result.text);
+                    console.log('message sent');
+                    e.target.reset();
+                    setErrors({});
+                    setIsValid(false);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
+
+    const validateForm = () => {
+        let formErrors = {};
+        let valid = true;
+
+        requiredFields.forEach(({ name, label }) => {
+            if (!form.current[name].value) {
+                formErrors[name] = `Please enter ${label.toLowerCase()}`;
+                valid = false;
+            }
+        });
+
+        setErrors(formErrors);
+        setIsValid(valid);
+        return valid;
+    };
+
+    const handleInputChange = () => {
+        validateForm();
+    };
+
     return (
-        <div className="text-[#404040] mx-auto w-10/12 mt-12 md:w-8/12 grid grid-cols-1 lg:columns-40-60-lg lg:mt-40">
+        <div
+            className={c(
+                'text-[#404040] mx-auto w-10/12 mt-12 md:w-8/12 grid grid-cols-1 lg:columns-40-60-lg lg:mt-40',
+                success && 'blurred'
+            )}>
+            <div
+                className={c(
+                    'bg-white w-96 h-20 shadow-xl rounded-lg absolute left-1/2 top-1/2 border -translate-x-1/2 -translate-y-1/2 flex flex-wrap content-center justify-center duration-300',
+                    success ? 'block' : 'hidden'
+                )}>
+                <p className="text-[22px] h-min">Successfully sent! </p>
+                <div className="w-8 h-8 ml-6">
+                    <BsFillCheckCircleFill className="fill-[green] inline w-full h-full" />
+                </div>
+            </div>
             <header>
                 <h2 className="font-medium">EMAIL</h2>
                 <p>fullstackjaden@gmail.com</p>
@@ -28,38 +134,60 @@ function ContactInfo() {
                 <p className="w-11/12 mx-auto mt-3">
                     Let's work together to build something great.
                 </p>
-                <div className="w-11/12 mx-auto mt-3">
+                <form
+                    ref={form}
+                    onSubmit={sendEmail}
+                    className="w-11/12 mx-auto mt-3">
                     <div className="grid grid-cols-1 md:grid-cols-2">
-                        <input
-                            type="text"
-                            className="my-2 border-b-[1px] pb-3 border-b-black outline-none w-full md:w-11/12"
-                            placeholder="First Name"
-                        />
-                        <input
-                            type="text"
-                            className="my-2 border-b-[1px] pb-3 border-b-black outline-none w-full"
-                            placeholder="Last Name"
-                        />
+                        {requiredFields.slice(0, 2).map(({ name, label }) => (
+                            <FormInput
+                                key={name}
+                                name={name}
+                                type="text"
+                                label={label}
+                                errors={errors}
+                                onChange={handleInputChange}
+                                className="w-full md:w-11/12"
+                            />
+                        ))}
                     </div>
-                    <input
-                        type="text"
-                        className="my-2 border-b-[1px] pb-3 border-b-black outline-none w-full"
-                        placeholder="Email Address"
-                    />
-                    <input
-                        type="text"
-                        className="my-2 border-b-[1px] pb-3 border-b-black outline-none w-full"
-                        placeholder="Phone Number"
-                    />
+                    {requiredFields.slice(2, 4).map(({ name, label }) => (
+                        <FormInput
+                            key={name}
+                            name={name}
+                            type={name === 'email_address' ? 'email' : 'tel'}
+                            label={label}
+                            errors={errors}
+                            onChange={handleInputChange}
+                            className="w-full"
+                        />
+                    ))}
                     <textarea
+                        onChange={handleInputChange}
+                        name="message"
                         type="text"
-                        className="my-2 border-b-[1px] pb-3 border-b-black outline-none w-full"
-                        placeholder="Message"
+                        className={c(
+                            `my-2 border-b-[1px] pb-3 ${
+                                errors.message
+                                    ? 'border-b-[#b3b3b3]'
+                                    : 'border-b-black'
+                            } outline-none w-full`
+                        )}
+                        placeholder={'Message'}
                     />
-                    <button className="w-full py-2 text-white bg-black">
-                        Get in touch
-                    </button>
-                </div>
+                    <input
+                        type="submit"
+                        value="Get in touch"
+                        className={`w-full py-2 text-white cursor-pointer duration-300 ${
+                            isValid ? 'bg-black' : 'bg-[#636363]'
+                        }`}
+                    />
+                    {showMissingFieldsMessage && (
+                        <p className="mt-2 text-red-500">
+                            Please fill out all fields
+                        </p>
+                    )}
+                </form>
             </div>
         </div>
     );
